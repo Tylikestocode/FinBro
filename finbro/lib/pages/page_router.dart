5d9becb0d1d5693924f3a6d26554813cd61debe5
Tyron <tyronlambrechts15@gmail.com>
2024-07-13 11:06:20 +0200

    changes

--- finbro/lib/pages/page_router.dart
+++ finbro/lib/pages/page_router.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:finbro/pages/ai.dart';
+import 'package:finbro/pages/analytics.dart';
+import 'package:finbro/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 class PageRouter extends StatefulWidget {
@@ -10,8 +13,23 @@ class PageRouter extends StatefulWidget {
 }
 
 class _PageRouter extends State<PageRouter> {
+  int Index = 0;
+  final List _pages = [HomePage(), AnalyticsPage(), AI()];
+
+  void SwitchPage(int index) {
+    setState(() {
+      Index = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: Index, onTap: SwitchPage, items: [
+            BottomNavigationBarItem(icon: icon)
+          ]),
+      body: _pages[index],
+    );
   }
 }
