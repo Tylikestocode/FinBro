f9206cc4ef7cf6c69cf64f62cf63f9183da8ced5
Yazeed Mohamed <yazeedmo0810@gmail.com>
2024-08-23 21:09:52 +0200

    Feature: UserDTO

---
+++ FinBroJavaSpring/src/main/java/com/finbro/FinBroJavaSpring/domain/UserDTO.java
@@ -0,0 +1,73 @@
+package com.finbro.FinBroJavaSpring.domain;
+
+import java.util.List;
+
+public class UserDTO {
+
+    private User user;
+    private List<Account> accounts;
+    private List<Transaction> transactions;
+    private List<Category> categories;
+    private List<RegularPayment> regularPayments;
+
+    public UserDTO() {}
+
+    public UserDTO(User user, List<Account> accounts, List<Transaction> transactions, List<Category> categories, List<RegularPayment> regularPayments) {
+        this.user = user;
+        this.accounts = accounts;
+        this.transactions = transactions;
+        this.categories = categories;
+        this.regularPayments = regularPayments;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public List<RegularPayment> getRegularPayments() {
+        return regularPayments;
+    }
+
+    public void setRegularPayments(List<RegularPayment> regularPayments) {
+        this.regularPayments = regularPayments;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "user=" + user +
+                ", accounts=" + accounts +
+                ", transactions=" + transactions +
+                ", categories=" + categories +
+                ", regularPayments=" + regularPayments +
+                '}';
+    }
+}
