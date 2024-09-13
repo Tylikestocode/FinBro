38acf357ade30b50556f3993347898451370e2ae
Yazeed Mohamed <yazeedmo0810@gmail.com>
2024-09-13 12:30:17 +0200

    Refactor: Folder Structure

--- FinBroJavaSpring/src/main/java/com/finbro/FinBroJavaSpring/exception/UserIDNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.finbro.FinBroJavaSpring.exception;
-
-public class UserIDNotFoundException extends RuntimeException {
-
-    public UserIDNotFoundException(String message) {
-        super(message);
-    }
-
-}
